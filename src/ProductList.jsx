95991cf472cacdff96b2c4f7a282c9efaeea176a
elaine1129 <elainelowjingyi00@gmail.com>
2025-07-29 05:42:43 -0400

    fixes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -235,6 +235,7 @@ function ProductList({ onHomeClick }) {
         color: 'white',
         fontSize: '30px',
         textDecoration: 'none',
+        position: 'relative',
     }
 
     const handleHomeClick = (e) => {
@@ -258,7 +259,6 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = (product) => {
-        console.log('addItem dispatch===')
         dispatch(addItem(product));
 
         setAddedToCart((prevState)=> ({
@@ -268,7 +268,7 @@ function ProductList({ onHomeClick }) {
     }
     
     const calculateTotalQuantity = () => {
-        return cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+        return cartItems.items ? cartItems.items.reduce((total, item) => total + item.quantity, 0) : 0;
     }
     return (
         <div>
@@ -287,7 +287,19 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1>
+                    {calculateTotalQuantity() > 0 && (
+                        <span
+                            style={{
+                                position: 'absolute',
+                                top: '50%',
+                                left: '50%',
+                                transform: 'translate(-50%, -50%)',
+                            }}
+                        >
+                            {calculateTotalQuantity()}
+                        </span>
+                    )}</a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -310,8 +322,9 @@ function ProductList({ onHomeClick }) {
                                         <div className="product-cost">${plant.cost}</div>
                                         <button
                                             className="product-button"
+                                            disabled={addedToCart[plant.name]}
                                             onClick={() => handleAddToCart(plant)}>
-                                                Add to Cart
+                                                {addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}
                                         </button>
                                     </div>
                                 ))}
